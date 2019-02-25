6046757b1c3fa79470fb8275f23bdd739b716fc8
Anten Skrabec <cebarks@gmail.com>
2019-02-25 09:52:49 -0700

    fixes test and adds vcr cassete

--- spec/requests/gifs_request_spec.rb
+++ spec/requests/gifs_request_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe "gifs API" do
-  it "weather gifs for a city" do
+  it "weather gifs for a city", :vcr do
     get '/api/v1/gifs?location=denver,co'
 
     expect(JSON.parse(response.body)).to have_key("copyright")
 
     res = parse_json
-    
-    expect(res).to have_key(:images)
-    expect(res[images]).to be_a(Array)
 
-    res[images].each do |image|
+    expect(res).to have_key(:images)
+    expect(res[:images]).to be_a(Array)
+
+    res[:images].each do |image|
       expect(image).to have_key(:time)
       expect(image).to have_key(:summary)
       expect(image).to have_key(:url)
